4218568758bd87c9077580f2666e9afbf6a5cacc
refrains <james@blair.nz>
2024-06-03 03:02:07 +0000

    fix bubble sort and refactor

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void readArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
 
 int main() {
     int n;
     cin >> n;
 
-    // read numbers O(n)
-    int nums[n];
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
+    int arr[n];
 
-    // bubble sort O(n^2)
-    int tmp;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
-            if (nums[i] > nums[j]) {
-                tmp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = tmp;
-            }
-        }
-    }
-
-    // print sorted numbers O(n)
-    for (int i = 0; i < n; i++) {
-        cout << nums[i] << " ";
-    }
-
-    cout << endl;
+    readArray(arr, n);
+    bubbleSort(arr, n);
+    printArray(arr, n);
 }
--- quick-sort.cpp
+++ quick-sort.cpp
@@ -1,57 +1,57 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int partition(vector<int>& nums, int lo, int hi) {
-    int pivot = nums[hi];
-    int index = lo - 1;
+int partition(int arr[], int lo, int hi) {
+    int pivot = arr[hi];
+    int idx = lo - 1;
 
     for (int i = lo; i < hi; i++) {
-        if (nums[i] <= pivot) {
-            index++;
-            swap(nums[i], nums[index]);
+        if (arr[i] <= pivot) {
+            idx++;
+            swap(arr[i], arr[idx]);
         }
     }
 
-    index++;
-    nums[hi] = nums[index];
-    nums[index] = pivot;
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
 
-    return index;
+    return idx;
 }
 
-void quickSort(vector<int>& nums, int lo, int hi) {
+void quickSort(int arr[], int lo, int hi) {
     if (lo >= hi) {
         return;
     }
 
-    int pivotIndex = partition(nums, lo, hi);
+    int pivotIdx = partition(arr, lo, hi);
 
-    quickSort(nums, lo, pivotIndex - 1);
-    quickSort(nums, pivotIndex + 1, hi);
+    quickSort(arr, lo, pivotIdx - 1);
+    quickSort(arr, pivotIdx + 1, hi);
+}
+
+void readArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
 }
 
 int main() {
     int n;
     cin >> n;
 
-    // read numbers O(n)
-    vector<int> nums;
-    int num;
+    int arr[n];
 
-    for (int i = 0; i < n; i++) {
-        cin >> num;
-        nums.push_back(num);
-    }
-
-    // quck sort
-    quickSort(nums, 0, nums.size() - 1);
-
-    // print sorted numbers O(n)
-    for (int n: nums) {
-        cout << n << " ";
-    }
-
-    cout << endl;
+    readArray(arr, n);
+    quickSort(arr, 0, n - 1);
+    printArray(arr, n);
 }
